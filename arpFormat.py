005a5e91f9931630e1788d97bb14a41099ac4345
Dean Sanders <Sandman2127@gmail.com>
2021-10-25 14:09:46 -0500

    Major development on v0.1, near functional on complete contigs with entire file runs

--- arpFormat.py
+++ arpFormat.py
@@ -1,2 +1,74 @@
 #!/usr/bin/env python3
 
+class arlequinForm:
+    def __init__(self,title,totalSamp):
+        self.sampTitle = title
+        self.noSamp = totalSamp
+        self.genoData = 1
+        self.gameticPhase = 0
+        self.dType = 'DNA'
+        self.missData = '\'N\''
+        self.locusSep = 'NONE'
+        self.profileString = "[Profile]\n" + "Title=" + "\"" + str(self.sampTitle) + "\"" + "\n" + "NbSamples=" + str(self.noSamp) + "\n" + "GenotypicData=" + str(self.genoData) + "\n" + "GameticPhase=" + str(self.gameticPhase) + "\n" + "DataType=" + str(self.dType) + "\n" + "MissingData=" + self.missData + "\n" + "LocusSeparator=" + str(self.locusSep) + "\n" + '[Data]' + "\n"
+        # [Profile]
+        #  Title="SNP data"
+        #  NbSamples=4
+        #  GenotypicData=1
+        # GameticPhase=0
+        #  DataType=DNA
+        # MissingData='N'
+        # LocusSeparator=NONE
+        # [Data]
+
+    def writeArpHeader(self,outputFileObject):
+        outputFileObject.write(self.profileString)
+
+    def writeSampleGroupData(self,groupName,SampData,SampCount,writeObj,outputFileObject):
+        if writeObj == "HEADER":
+            header = "[[Samples]]" + "\n" + "SampleName=" + "\"" + str(groupName) + "\"" + "\n" + "SampleSize=" + str(SampCount) + "\n" + "SampleData= {\n"
+            outputFileObject.write(header)
+        elif writeObj == "TAIL":
+            tail = "\n}\n"
+            outputFileObject.write(tail)
+        elif writeObj == "SAMPLE":
+            sampleLine = ""
+            for sample,genotype in SampData.items():
+                sampleLine = sampleLine + "\t" + str(sample) + "\t" + str(1) + "\t" + str(genotype[0]) + "\n" + "\t\t\t" + str(genotype[1]) + "\n"
+            outputFileObject.write(sampleLine)
+        # tail = "\n}\n"
+        #  [[Samples]]
+        # SampleName="Ontario"
+        # SampleSize=6
+        # SampleData= {
+        # 	ONT0102D	1	AAATGAAGTGGGAGGTTCCGG
+        # 			AACTGAAGTGGGAGGTTCCGG
+        # 	ONT0402D	1	AACTGAAGTGGGAGGTTCCGG
+        # 			AACTGAAGTGGGAGGTTCCGG
+        # 	ONT0702	1	AAATGAAGTTGGACGTTTCGA
+        # 			AAATGAAGTTGGACGTTTCGA
+        # 	ONT0902D	1	AAATGAAGTTGGACGTTTCGG
+        # 			AAATGAAGTTGGACGTTTCGG
+        # 	ONT1102	1	AAATGAAGTTGGACGTTTCGA
+        # 			AAATGAAGTTGGACGTTTCGA
+        # 	ONT1102D	1	AACTGAAGTTGGACGTTTCGG
+        # 			AACTGAAGTTGGACGTTTCGG
+
+        #}
+        
+    def writeFinalSampleGroups(self,groups,outputFileObject):
+        header = '[[Structure]]' + '\n' + 'StructureName="A group of ' + str(len(groups)) + ' populations"' + "\n" + 'NbGroups=1' + "\n" + "Group= {" + "\n"
+        FinalGroups = ""
+        for group in groups:
+            FinalGroups = FinalGroups + "\"" + str(group) + "\"" + "\n"
+        FinalGroupString = header + FinalGroups + "}" + "\n"
+        outputFileObject.write(FinalGroupString)
+
+        # [[Structure]]
+        #  StructureName="A group of n populations analyzed for DNA"
+        #  NbGroups=1
+        #  Group= {
+        # "Alberta"
+        # "BC"
+        # "Quebec"
+        # "Ontario"
+        #  }
--- vcf2ArlequinDiploid.py
+++ vcf2ArlequinDiploid.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse, os
-import arpFormat
+import arpFormat as arp
 
 parser = argparse.ArgumentParser(description='UWBC BRC tool for Tassel V2 report building')
 parser.add_argument('--vcf', type=str,help='The path to the vcf to convert to arlequin format', required=True)
@@ -12,27 +12,131 @@ vcf = args.vcf
 popfile = args.popFile
 cwd = os.getcwd()
 
-
 def findChromLine():
     count = 0
+    chrLineData = []
     with open(vcf,'r') as f:
         while True:
             line = f.readline()
             count = count + 1
             if line[0:6] == '#CHROM':
-                return count
+                chrLineData = [line,count]
+                return chrLineData
 
-def processGenoField():
-    pass
+def generatePopMap():
+    population_map = {}
+    populations = []
+    with open(popfile,'r') as popF:
+        lines = popF.readlines()
+        for line in lines:
+            line = line.split()
+            sample = str(line[0])
+            samplePop = str(line[1])
+            population_map[sample] = samplePop
+            populations.append(samplePop)
+    populationsOut = set(populations)
+    popMap_popList = [population_map,populationsOut]
+    return popMap_popList
 
-def parseVCF():
-    pass
+def findSampCount(population,popMap):
+    sampCNT = 0
+    for samp,sampPop in popMap.items():
+        if population == sampPop:
+            sampCNT += 1
+    return sampCNT
+
+def findSampleColumn(chrLine,sample):
+    pos = 0
+    for field in chrLine.split("\t"):
+        pos += 1
+        if str(field) == str(sample):
+            # corrects for list[0] splitting of vcf file
+            pos = pos - 1
+            return pos
+
+def processGenoField(ref,alt,geno):
+    gtOut = []
+    geno1 = geno[0]
+    geno2 = geno[2:3]
+    if geno1 == '.' and geno2 == '.':
+        gtOut = ['N','N']
+    elif geno1 == '0' and geno2 == '0':
+        gtOut = [ref,ref]
+    elif geno1 == '0' and geno2 == '1':
+        gtOut = [ref,alt]
+    elif geno1 == '1' and geno2 == '0':
+        gtOut = [alt,ref]
+    elif geno1 == '1' and geno2 == '1':
+        gtOut = [alt,alt]
+    return gtOut
+
+def parseVCF(fieldPos,lineSkip):
+    genotypeList = []
+    genotypeA = "" 
+    genotypeB = ""
+    count = 0
+    with open(vcf,'r') as f:
+        while True:
+            line = f.readline()
+            if line:
+                count += 1
+                if count <= lineSkip:
+                    # vcf header lines
+                    pass
+                else:
+                    refAllele = str(line.split("\t")[3])
+                    altAllele = str(line.split("\t")[4])
+                    # prevents alternate allele indels from sneaking into the analysis 
+                    if altAllele == '.':
+                        pass
+                    else:
+                        genoTypeField = str(line.split("\t")[fieldPos])
+                        alleleA,alleleB = processGenoField(refAllele,altAllele,genoTypeField)
+                        genotypeA = genotypeA + alleleA
+                        genotypeB = genotypeB + alleleB
+            else:
+                break
+    genotypeList = [genotypeA,genotypeB]
+    return genotypeList
 
 def main():
-    chromLine = findChromLine()
-    #print(chromLine)
-    pass
+    #TODO: parse popFile and find location of sample in chromLine:
+    popMap,popList = generatePopMap()
+    print("[STDOUT]: your populations are:",list(popList))
+    print("[STDOUT]: your population map indicates the following mapping:",popMap)
+    
+    #TODO: write out the header of our arp file
+    arpInst = arp.arlequinForm("Arlequin SNP analysis",len(popMap))
+    outputARP = open("output.arp",'w+')
+    arpInst.writeArpHeader(outputARP)
 
+    #TODO: parseVCF by sample group --> sample:
+    chromLine,chromLineNo = findChromLine()
+    for pop in popList:
+        print("[STDOUT]: converting data from the population:",str(pop))
+        #TODO: write out new sample group header for each population
+        nullDict = {"Must":["Pass","Something"]}
+        sampCNT = findSampCount(pop,popMap)
+        arpInst.writeSampleGroupData(pop,nullDict,sampCNT,"HEADER",outputARP)
+        #TODO: parse for samples in the correct population and write them out as we find them
+        for samp,sampPop in popMap.items():
+            if pop == sampPop:
+                print("[STDOUT]: converting sample data from the sample:",str(samp),"population:",str(sampPop))
+                sampColumn = findSampleColumn(chromLine,samp)
+                if sampColumn:
+                    genotypeA,genotypeB = parseVCF(sampColumn,chromLineNo)
+                    sampleData = {samp:[genotypeA,genotypeB]}
+                    #TODO: write out sample data for each individual in the population
+                    arpInst.writeSampleGroupData(pop,sampleData,sampCNT,"SAMPLE",outputARP)
+                else:
+                    print("[STDOUT]: sample data doesnt exist for sample:",str(samp),"population:",str(sampPop),"within vcf:",str(vcf))
+            else:
+                pass
+        #TODO: write out the tail once all samples in the population complete
+        arpInst.writeSampleGroupData(pop,nullDict,sampCNT,"TAIL",outputARP)
+
+    #TODO: write out the sample group information at the end of the ARP file:
+    arpInst.writeFinalSampleGroups(popList,outputARP)
 
 
 if __name__ == '__main__':
